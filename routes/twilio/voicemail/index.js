98acb209acd9e4b8c4ac4c13974effbb4a27164b
AJ ONeal <coolaj86@gmail.com>
2013-07-22 23:34:15 +0000

    all seems to work

--- routes/twilio/voicemail/index.js
+++ routes/twilio/voicemail/index.js
@@ -8,7 +8,7 @@
     , realsendemail
     ;
 
-  function forwardVoicemailViaEmail(caller, mp3, body) {
+  function forwardVoicemailViaEmail(caller, mp3, text, body) {
     var subject
       , msg
       ;
@@ -16,7 +16,8 @@
     subject = "Voicemail from " + caller ;
     msg = ""
       + "\n" + caller + "\n\n"
-      + mp3
+      + mp3 + "\n\n"
+      + (text ? (text + "\n\n") : '')
       + "\n\n\n\n"
       + body
       ;
@@ -24,14 +25,19 @@
     realsendemail(config.forwardEmailTo, subject, msg);
   }
 
-  function forwardVoicemailViaSms(caller, mp3) {
-    // Use this convenient shorthand to send an SMS:
+  function forwardVoicemailViaSms(caller, mp3, text) {
+    var obj
+      ;
+
+    obj = {
+      to: config.forwardTo
+    , from: config.number
+            //             15 +   12   +  1  + 122 = 150
+    , body: "Voicemail from " + caller + " " + mp3 + (text ? (" " + text) : "")
+    };
+
     twilio.sendSms(
-      { to: config.forwardTo
-      , from: config.number
-              //             15 +   12   +  1  + 122 = 150
-      , body: "Voicemail from " + caller + " " + mp3
-      }
+      obj
     , function(error, message) {
         if (!error) {
           console.log('Success! The SID for this SMS message is:');
@@ -60,7 +66,17 @@
 
     //resp.play('http://chunkhost.coolaj86.com:8080/voicemail.wav');
     resp.play('/voicemail.wav');
-    resp.record({ action: '/twilio/voicemail/forward', maxLength: 150 }); //, transcribe: true });
+    resp.record({
+      action: '/twilio/voicemail/forward?wait=true'
+    , transcribeCallback: '/twilio/voicemail/forward'
+    , maxLength: 150
+    , transcribe: true
+      // TODO remove beep from my recording
+    , playBeep: false
+      // end recording if the person doesn't speak within 5 seconds
+    , timeout: 5
+    , method: 'POST'
+    });
 
     // Actually respond
     res.setHeader('Content-Type', 'application/xml');
@@ -75,11 +91,30 @@
     // Both of these methods are simply best-effort (and no callback).
     // TODO Could store them in a database and retry or something, but keeping it simple for now
     // RecordingUrl: 'http://api.twilio.com/2010-04-01/Accounts/AC08e198a865cfa0bada6c2b7d5b41b02a/Recordings/REbd0c0c8a0c552687959102baac212933'
-    forwardVoicemailViaSms(req.body.Caller, req.body.RecordingUrl);
-    forwardVoicemailViaEmail(req.body.Caller, req.body.RecordingUrl, JSON.stringify(req.body, null, '  '));
+
+    // Don't do anything until the transcription is complete
+    if (req.query.wait) { 
+      res.setHeader('Content-Type', 'application/xml');
+      res.write(resp.toString());
+      res.end();
+      return;
+    }
 
     res.setHeader('Content-Type', 'application/xml');
     res.write(resp.toString());
     res.end();
+
+    // Recording + Transcript
+    forwardVoicemailViaSms(
+      req.body.Caller
+    , req.body.RecordingUrl
+    , req.body.TranscriptionText
+    );
+    forwardVoicemailViaEmail(
+      req.body.Caller
+    , req.body.RecordingUrl
+    , req.body.TranscriptionText
+    , JSON.stringify(req.body, null, '  ')
+    );
   };
 }());
