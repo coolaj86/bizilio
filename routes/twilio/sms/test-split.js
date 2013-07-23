acd849f27c83727b401a1ee107f3bbfb81435e5d
AJ ONeal <coolaj86@gmail.com>
2013-07-23 18:34:52 +0000

    updated tests and they still pass, woohoo!

--- routes/twilio/sms/test-split.js
+++ routes/twilio/sms/test-split.js
@@ -6,17 +6,20 @@ var tests
 
 tests = [
   // 0
+  // doesn't break when the message length is exact on a word boundary
   { msg: "Just need a message that's a good 154 characters long and it's okay if it doesn't make sense as long as it ends with the word ethylenediaminetetraacetates"
   , result: ["Just need a message that's a good 154 characters long and it's okay if it doesn't make sense as long as it ends with the word ethylenediaminetetraacetates"]
   }
 
   // 1
+  // doesn't break when the message length is exact, even with non-word boundaries
 , { msg: "Just need a message that's a bad 154 characters long and it's okay if it doesn't make sense as long as it ends with the word ethylenediaminetetraacetates."
   , result: ["Just need a message that's a bad 154 characters long and it's okay if it doesn't make sense as long as it ends with the word ethylenediaminetetraacetates."]
   }
 
 
   // 2
+  // breaks on a word boundary
 , { msg: "Just need a message that's a good 154 characters long and it's okay if it doesn't make sense as long as it ends with the word ethylenediaminetetraacetates and then some stuff"
   , result: [
       "Just need a message that's a good 154 characters long and it's okay if it doesn't make sense as long as it ends with the word ethylenediaminetetraacetates"
@@ -26,6 +29,7 @@ tests = [
 
 
   // 3
+  // breaks after a word boundary
 , { msg: "Just need a message that's a bad 154 characters long and it's okay if it doesn't make sense as long as it ends with the word ethylenediaminetetraacetates and then some stuff"
   , result: [
       "Just need a message that's a bad 154 characters long and it's okay if it doesn't make sense as long as it ends with the word ethylenediaminetetraacetates"
@@ -35,21 +39,36 @@ tests = [
 
 
   // 4
+  // breaks before a word boundary
 , { msg: "Just need a message that's a good 154 characters long and it's okay if it does not make sense as long as it ends with the word ethylenediaminetetraacetates and then some stuff"
   , result: [
       "Just need a message that's a good 154 characters long and it's okay if it does not make sense as long as it ends with the word"
     , "ethylenediaminetetraacetates and then some stuff"
     ]
   }
+
+
+  // 5
+  // this tests a long message that breaks on a non-word character and removes the extra space
 , { msg: "This is a really long message and that's okay, I'd prefer it long any ol' way. Lalalala Ahuramazda! And something else in turkish murkish lurkish durkish. Hey mimi. You can't read this because you're over there. It's a long message."
   , result: [
       "This is a really long message and that's okay, I'd prefer it long any ol' way. Lalalala Ahuramazda! And something else in turkish murkish lurkish durkish."
     , "Hey mimi. You can't read this because you're over there. It's a long message."
     ]
   }
+
+
+  // 6
+  // This tests hyphenation
+, { msg: "Just need a message that's a good 154 characters long and it's okay if it doesn't make sense as long as it ends with the word omylolzethylenediaminetetraacetates and then some stuff"
+  , result: [
+      "Just need a message that's a good 154 characters long and it's okay if it doesn't make sense as long as it ends with the word omylolzethylenediaminetetra-"
+    , "acetates and then some stuff"
+    ]
+  }
+
+  // needs test for removing excess newlines and spaces
 ];
-
-
 
 tests.forEach(function (test, t) {
   var result
--- routes/twilio/sms/utils.js
+++ routes/twilio/sms/utils.js
@@ -12,10 +12,11 @@
       , subchars
       , MAX_WORD_LEN = 28
       , minLen = len - MAX_WORD_LEN
+      , broken
       ;
 
-    function push(len) {
-      subchars = chars.splice(0, len).join('');
+    function push(len, char) {
+      subchars = chars.splice(0, len).join('') + (char || '');
       if (subchars) {
         bodies.push(subchars);
       }
@@ -23,6 +24,7 @@
 
     // TODO try to split on a word boundary
     while (chars.length) {
+      broken = false;
       // remove leading space
       // (trailing space is okay though)
       while (' ' === chars[0]) {
@@ -38,10 +40,19 @@
       // there are legitimate english words up to 28 characters long
       // http://www.morewords.com/wordsbylength/
       // #AndPerhapsMoreCommonlyThisKindOfCrap
-      for (i = len; i > minLen; i -= 1) {
+      for (i = len; i >= minLen; i -= 1) {
         if (/\W/.test(chars[i])) {
+          broken = true;
           break;
         }
+      }
+
+      // if we can't reasonably split on a word boundary,
+      // might as well get our money's worth!
+      if (!broken) {
+        i = len;
+        push(i - 1, '-');
+        continue;
       }
 
       push(i);
