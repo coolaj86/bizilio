013c957d15e910aabd59d4e68515f86aeb20619a
AJ ONeal <coolaj86@gmail.com>
2013-07-23 14:00:31 +0000

    got email form working too?

--- app.js
+++ app.js
@@ -6,10 +6,11 @@
 
 var express = require('express')
   , twilio = require('./routes/twilio')
+  , email = require('./routes/email')
   , http = require('http')
   , path = require('path')
   , app = express()
-  , port = process.env.PORT || 3000
+  , port = process.argv[2] || process.env.PORT || 3000
   , mailer = require('./lib/mailer')
   ;
 
@@ -21,7 +22,7 @@ app.use(express.favicon());
 app.use(express.logger('dev'));
 // only urlencoded forms need csrf, I think
 // ditch bodyParser in favor of json only parser
-//app.use(express.json());
+app.use(express.json());
 app.use(express.urlencoded());
 app.use(express.methodOverride());
 app.use(app.router);
@@ -42,11 +43,16 @@ if ('development' === app.get('env')) {
 mailer.init({
   config: require('./config.mailer.json')
 });
+email.init({
+  config: require('./config.email.json')
+, inquire: mailer.inquire
+});
 twilio.init({
   config: require('./config.twilio.json')
 , mail: mailer.mail
 /*TODO , mount: '/twilio'*/
 });
+
 
 // Incoming SMS
 app.post('/twilio/sms/forward', twilio.sms.forward);
@@ -55,12 +61,14 @@ app.post('/twilio/sms/forward', twilio.sms.forward);
 app.post('/twilio/voice', twilio.voice.create);
 app.post('/twilio/voice/screen', twilio.voice.screen);
 app.post('/twilio/voice/connect', twilio.voice.connect);
+//app.post('/twilio/voice/forward', twilio.voice.forward);
 
 // Voicemail
 app.post('/twilio/voicemail', twilio.voicemail.create);
 app.post('/twilio/voicemail/forward', twilio.voicemail.forward);
 
-
+// Email
+app.post('/email', email.create);
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
---
+++ routes/email/index.js
@@ -0,0 +1,53 @@
+(function () {
+  "use strict";
+
+  var email = module.exports
+    , config
+    , realsendemail
+    ;
+
+  function sendEmail(opts) {
+    realsendemail(
+      config.forwardTo  // to
+    , opts.replyTo      // replyTo
+    , opts.subject      // subject
+    , opts.text         // text
+    );
+  }
+
+  email.init = function (opts) {
+    config = opts.config;
+    realsendemail = opts.inquire;
+  };
+
+  // TODO add verimail
+  email.create = function (req, res) {
+    console.log('content-type', req.headers['content-type']);
+    if (/urlencoded/.test(req.headers['content-type'])) {
+      //next();
+      res.end('the s is for sucks');
+    }
+
+    var obj = {}
+      , name = (req.body.name || '').replace(/\s+/g, ' ')
+      , addr = (req.body.email || '').replace(/\s+/g, ' ')
+      , phone = (req.body.phone || '').replace(/\s+/g, ' ')
+      , subject = "Web msg from " + (addr || phone)
+      , text = (req.body.msg || '') + '\n' + name + '\n' + addr + "\n" + phone
+      ;
+
+    if (/^\S+@\S+\.\S+/.test(addr)) {
+      obj.replyTo = addr;
+    }
+
+    obj.subject = subject;
+    obj.text = text;
+    sendEmail(obj, function (err, resp) {
+      res.send({
+        "success": !err
+      , "errors": err && [err.toString()] || []
+      , "result": resp
+      });
+    });
+  };
+}());
