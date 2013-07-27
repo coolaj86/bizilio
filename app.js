478ff3c01833cf57ff8616c2e17a3119cd5a6cd5
AJ ONeal <coolaj86@gmail.com>
2013-07-27 05:15:48 +0000

    add compress

--- app.js
+++ app.js
@@ -25,6 +25,7 @@ app.use(express.logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded());
 app.use(express.methodOverride());
+app.use(express.compress());
 app.use(app.router);
 app.use(express.static(path.join(__dirname, 'public')));
 
